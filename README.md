cdba43764d7fc1a1451fa1d7da621430c4babecd
J.R. Bédard <jrbedard@gmail.com>
2014-12-08 07:59:28 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+ether-transactions
+===============
+
+Explore transactions on the Ethereum network discovered by Etherface.
+http://ether.fund/tool/transactions
---
+++ ether.transaction.js
@@ -0,0 +1,31 @@
+/*
+- ether.transaction.js v0.1
+- Explore transactions on the Ethereum network discovered by Etherface.
+- http://ether.fund/tool/transactions
+- (c) 2014 J.R. Bédard (jrbedard.com)
+*/
+
+
+// Init
+$(function() {
+
+	
+	
+});
+
+
+
+// list
+function getTransactions() {
+}
+
+
+// table
+function displayTransactionsTable(transactions) {
+}
+
+
+// view
+function getTransaction() {
+	
+}
